f2e9dfcf4b256675a696a40b60365963ad729619
nicolas.chalet@gmail.com <nicolas.chalet@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-03-25 11:08:19 +0000

    Ajout ProduitEnCommande

---
+++ trunk/VinsSurVinsBeansTemp/src/fr/afcepf/atod17/entitybeans/commande/ProduitEnCommandePK.java
@@ -0,0 +1,39 @@
+package fr.afcepf.atod17.entitybeans.commande;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class ProduitEnCommandePK implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name="idProduit")
+	private int idProduit;
+	
+	@Column(name="idCommande")
+	private int idCommande;
+	
+	public ProduitEnCommandePK() {
+		
+	}
+
+	public int getIdProduit() {
+		return idProduit;
+	}
+
+	public void setIdProduit(int idProduit) {
+		this.idProduit = idProduit;
+	}
+
+	public int getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdCommande(int idCommande) {
+		this.idCommande = idCommande;
+	}
+	
+}
