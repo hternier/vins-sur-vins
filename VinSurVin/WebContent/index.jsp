0a8729e49e754c078db8401c04ef8ad9ad347140
nicolas.chalet@gmail.com <nicolas.chalet@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-03-27 15:05:53 +0000

    Ajout de spring ainsi qu'un exemple

--- VinSurVin/WebContent/index.jsp
+++ VinSurVin/WebContent/index.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="f"  uri="http://java.sun.com/jsf/core"%>
 <%@ taglib prefix="h"  uri="http://java.sun.com/jsf/html"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -10,11 +10,11 @@
 <body>
 <f:view>
 <h:form>
-	<h:panelGroup layout="block">
+	<h:panelGroup rendered="#{mbTest.listeProduitLength > 0}">
 		<h:outputText value="#{mbTest.listeProduitLength }" /><br/>
 		<h:dataTable value="#{mbTest.listeProduit }" var="produit">
 			<h:column>
-				<f:facet name="header">Id</f:facet>
+				<f:facet name="header"><h:outputText value="Id" /></f:facet>
 				<h:outputText value="#{produit.id}" />
 			</h:column>
 		</h:dataTable>
--- VinSurVin/src/fr/afcepf/atod17/vinsurvin/control/managedbeans/ManagedBeanTest.java
+++ VinSurVin/src/fr/afcepf/atod17/vinsurvin/control/managedbeans/ManagedBeanTest.java
@@ -3,11 +3,10 @@ package fr.afcepf.atod17.vinsurvin.control.managedbeans;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import fr.afcepf.atod17.vinsurvin.dao.DaoTest;
 import fr.afcepf.atod17.vinsurvin.entitybeans.produit.Produit;
 
 public class ManagedBeanTest {
@@ -15,16 +14,10 @@ public class ManagedBeanTest {
 	private List<Produit> listeProduit = new ArrayList<Produit>();
 	
 	public String testAction() {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("VinSurVin");
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("test.xml");
 		
-		this.listeProduit = em.createQuery("From Produit", Produit.class).getResultList();
+		this.listeProduit = ctx.getBean("testBean", DaoTest.class).getAll();
 		
-		tx.commit();
-		em.close();
-		emf.close();
 		return "";
 	}
 
