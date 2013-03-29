9aada2c742e8c962e1a17d2b8695c7c8ec398818
saroui.nassim@gmail.com <saroui.nassim@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-03-29 10:29:59 +0000

    Création de la DAO, du service et du managed bean du Compte client

---
+++ trunk/VinSurVin/src/fr/afcepf/atod17/vinsurvin/services/interfaces/IServiceCompte.java
@@ -0,0 +1,9 @@
+package fr.afcepf.atod17.vinsurvin.services.interfaces;
+
+import fr.afcepf.atod17.vinsurvin.entitybeans.compte.CompteClient;
+
+public interface IServiceCompte {
+	
+	public CompteClient addCompteClient(CompteClient compte);
+
+}
