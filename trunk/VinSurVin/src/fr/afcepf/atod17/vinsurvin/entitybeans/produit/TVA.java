905ba1f8411c7da8cabfdc96fc6e4429a324cee9
nicolas.chalet@gmail.com <nicolas.chalet@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-03-29 08:38:01 +0000

    Suppression des champs DateDebut et Fin

--- trunk/VinSurVin/src/fr/afcepf/atod17/vinsurvin/entitybeans/produit/TVA.java
+++ trunk/VinSurVin/src/fr/afcepf/atod17/vinsurvin/entitybeans/produit/TVA.java
@@ -1,15 +1,12 @@
 package fr.afcepf.atod17.vinsurvin.entitybeans.produit;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 public class TVA implements Serializable {
@@ -21,14 +18,6 @@ public class TVA implements Serializable {
 	@Column(name="idTVA")
 	private int id;
 	
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name="dateDebutTVA")
-	private Date dateDebut;
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name="dateFinTVA")
-	private Date dateFin;
-	
 	@Column(name="valeurTVA")
 	private double valeur;
 
@@ -38,22 +27,6 @@ public class TVA implements Serializable {
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public Date getDateDebut() {
-		return dateDebut;
-	}
-
-	public void setDateDebut(Date dateDebut) {
-		this.dateDebut = dateDebut;
-	}
-
-	public Date getDateFin() {
-		return dateFin;
-	}
-
-	public void setDateFin(Date dateFin) {
-		this.dateFin = dateFin;
 	}
 
 	public double getValeur() {
