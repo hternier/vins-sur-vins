544da55db66b97a8be1f0c4d5da84a4747ec2546
saroui.nassim@gmail.com <saroui.nassim@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-04-02 08:16:00 +0000

    mise a jour managedbean creer compte

--- VinSurVin/src/fr/afcepf/atod17/vinsurvin/control/managedbeans/ManagedBeanCreerCompte.java
+++ VinSurVin/src/fr/afcepf/atod17/vinsurvin/control/managedbeans/ManagedBeanCreerCompte.java
@@ -1,16 +1,28 @@
 package fr.afcepf.atod17.vinsurvin.control.managedbeans;
 
+import fr.afcepf.atod17.vinsurvin.entitybeans.compte.Adresse;
 import fr.afcepf.atod17.vinsurvin.entitybeans.compte.CompteClient;
 import fr.afcepf.atod17.vinsurvin.services.implementations.ServiceCompteImpl;
 
 public class ManagedBeanCreerCompte extends AbstractManagedBean {
 
-	private CompteClient compteclient = new CompteClient();
+	private CompteClient compteClient = new CompteClient();
+
+	{
+		compteClient.setAdresseFacturation(new Adresse());
+	}
+
 	public String creerCompte() {
-		this.compteclient= getContext().getBean(ServiceCompteImpl.class).addCompteClient(compteclient);
-		System.out.println("c fait!");
+		
+		this.compteClient = getContext().getBean(ServiceCompteImpl.class).addCompteClient(compteClient);
 		return "Success";
 	}
-	
-	
+
+	public CompteClient getCompteClient() {
+		return compteClient;
+	}
+
+	public void setCompteClient(CompteClient compteClient) {
+		this.compteClient = compteClient;
+	}
 }
