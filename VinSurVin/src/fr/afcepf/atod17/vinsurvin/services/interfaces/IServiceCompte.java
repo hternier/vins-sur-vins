113d1ad5b6392f985df092c0fa46d2c8444eaf27
saroui.nassim@gmail.com <saroui.nassim@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-04-03 09:51:40 +0000

    ajout de getVilleParCP

--- VinSurVin/src/fr/afcepf/atod17/vinsurvin/services/interfaces/IServiceCompte.java
+++ VinSurVin/src/fr/afcepf/atod17/vinsurvin/services/interfaces/IServiceCompte.java
@@ -1,25 +1,39 @@
 package fr.afcepf.atod17.vinsurvin.services.interfaces;
 
+import java.util.List;
+
 import fr.afcepf.atod17.vinsurvin.entitybeans.compte.CompteAbstrait;
 import fr.afcepf.atod17.vinsurvin.entitybeans.compte.CompteClient;
 import fr.afcepf.atod17.vinsurvin.entitybeans.compte.CompteSysteme;
+import fr.afcepf.atod17.vinsurvin.entitybeans.compte.Ville;
 
 /**
- * @author Nassim 29/03/2013
- * Interface de gestion des compte utilisateurs et systeme.
- *
+ * @author Nassim 29/03/2013 Interface de gestion des compte utilisateurs et
+ *         systeme.
+ * 
  */
 public interface IServiceCompte {
 
 	public CompteClient addCompteClient(CompteClient compte);
-	public CompteClient getCompteClient(CompteClient compte);
-	public CompteClient authentificationFO (CompteAbstrait paramCompte);
 
+	public CompteClient getCompteClient(CompteClient compte);
 	
-	/** 
+	public List<Ville> getVilleParCP(String cp);
+
+	/**
+	 * Methode pour authentifier un utilisateur de site
+	 * 
+	 * @param paramCompteSysteme
+	 *            : objet compte systeme
+	 * **/
+	public CompteClient authentificationFO(CompteAbstrait paramCompte);
+
+	/**
 	 * Methode pour authentifier un gestionnaire de site
-	 * @param paramCompteSysteme : objet compte systeme
+	 * 
+	 * @param paramCompteSysteme
+	 *            : objet compte systeme
 	 * **/
 	public CompteSysteme authentificationBO(CompteSysteme paramCompteSysteme);
-	
+
 }
