d2ea8c86084d220e48f248715e325f3c2fd6e7cc
saroui.nassim@gmail.com <saroui.nassim@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-04-03 09:50:32 +0000

    ajout de getVilleParCP

--- VinSurVin/src/fr/afcepf/atod17/vinsurvin/dao/interfaces/compte/IDaoCompte.java
+++ VinSurVin/src/fr/afcepf/atod17/vinsurvin/dao/interfaces/compte/IDaoCompte.java
@@ -1,13 +1,20 @@
 package fr.afcepf.atod17.vinsurvin.dao.interfaces.compte;
 
+import java.util.List;
+
 import fr.afcepf.atod17.vinsurvin.entitybeans.compte.CompteClient;
 import fr.afcepf.atod17.vinsurvin.entitybeans.compte.CompteSysteme;
+import fr.afcepf.atod17.vinsurvin.entitybeans.compte.Ville;
 
 public interface IDaoCompte {
-	
+
 	public CompteClient setCompte(CompteClient compte);
-	  public CompteClient getCompteClient(CompteClient paramCompteClient);
-	
+
+	public CompteClient getCompteClient(CompteClient paramCompteClient);
+
 	public CompteSysteme authentificationBO(String login, String mdp);
-    public CompteClient authentificationFO(String paramMail, String paramMdp);
+
+	public CompteClient authentificationFO(String paramMail, String paramMdp);
+	
+	public List<Ville> getVilleParCP(String cp);
 }
