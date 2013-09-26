8372663d909c71b47a58556ddc053a4cef36f17d
hadrien.ternier@gmail.com <hadrien.ternier@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-09-26 12:30:20 +0000

    Déploiement entitée gestionStockInterne commun

---
+++ gestionStockInterne/gestionStockInterne-commun/src/main/java/fr/afcepf/atod18/gestionStockInterne/commun/entitees/ProduitStock.java
@@ -0,0 +1,49 @@
+package fr.afcepf.atod18.gestionStockInterne.commun.entitees;
+
+/**
+ * Entité pour la gestion de stock.
+ * Utilisée pour les Web services.
+ * @author Administrateur
+ */
+public class ProduitStock {
+
+	/**
+	 * Identifiant du produit
+	 */
+	private Integer id;
+
+	/**
+	 * Quantité du produit dans le stock
+	 */
+	private Integer quantiteStock;
+
+	/**
+	 * Quantité minimal à partire de laquelle le stock doit être réaprovisionné
+	 */
+	private Integer quantiteMinimal;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer paramId) {
+		id = paramId;
+	}
+
+	public Integer getQuantiteStock() {
+		return quantiteStock;
+	}
+
+	public void setQuantiteStock(Integer paramQuantiteStock) {
+		quantiteStock = paramQuantiteStock;
+	}
+
+	public Integer getQuantiteMinimal() {
+		return quantiteMinimal;
+	}
+
+	public void setQuantiteMinimal(Integer paramQuantiteMinimal) {
+		quantiteMinimal = paramQuantiteMinimal;
+	}
+
+}
