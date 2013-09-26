96c4e65f7510a6c33daa2e97d6f4434e093e4063
hadrien.ternier@gmail.com <hadrien.ternier@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-09-26 13:17:53 +0000

    Modification du fonctionnement du ws : n'utilise plus d'objets mais des integers

--- trunk/gestionStockInterne/gestionStockInterne-commun/src/main/java/fr/afcepf/atod18/gestionStockInterne/commun/webService/StockInterneService.java
+++ trunk/gestionStockInterne/gestionStockInterne-commun/src/main/java/fr/afcepf/atod18/gestionStockInterne/commun/webService/StockInterneService.java
@@ -3,14 +3,11 @@ package fr.afcepf.atod18.gestionStockInterne.commun.webService;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-import fr.afcepf.atod18.gestionStockInterne.commun.entitees.ProduitStock;
-
-
 @WebService
 public interface StockInterneService {
 
 	@WebMethod(operationName="getStock")
-	public ProduitStock getStock(ProduitStock paramProduitStock);
+	public Integer getStock(Integer paramIdProduit);
 	
 	@WebMethod(operationName="ping")
 	public String ping(String paramString);
