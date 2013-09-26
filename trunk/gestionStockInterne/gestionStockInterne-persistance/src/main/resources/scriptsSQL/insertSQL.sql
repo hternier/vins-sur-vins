92b450d54ce57be95882ef21c475911fc5a75650
hadrien.ternier@gmail.com <hadrien.ternier@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-09-26 14:07:13 +0000

    Création script insertion des données pour gestion stock interne

---
+++ trunk/gestionStockInterne/gestionStockInterne-persistance/src/main/resources/scriptsSQL/insertSQL.sql
@@ -0,0 +1,9 @@
+use atod18_g1_geststockint;
+
+insert into `produitstock` values(1,34,30);
+insert into `produitstock` values(2,24,30);
+insert into `produitstock` values(3,256,5);
+insert into `produitstock` values(4,30,15);
+insert into `produitstock` values(5,10,30);
+insert into `produitstock` values(6,253,30);
+insert into `produitstock` values(7,300,30);
