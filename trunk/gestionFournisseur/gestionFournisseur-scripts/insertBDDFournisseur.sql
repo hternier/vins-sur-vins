e4a30424e1651266067a7153efee13657fe6ced8
CinhicM@gmail.com <CinhicM@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-09-30 08:36:20 +0000

    Déplacement des scripts sql

--- trunk/gestionFournisseur/gestionFournisseur-scripts/insertBDDFournisseur.sql
+++ trunk/gestionFournisseur/gestionFournisseur-scripts/insertBDDFournisseur.sql
@@ -1,5 +1,13 @@
 use atod18_g1_gestionfournisseur;
 
+INSERT INTO `fournisseur` (`IDFOURNISSEUR`,`LIBELLEFOURNISSEUR`) VALUES 
+ (1,'Chantovent'),
+ (2,'Castel'),
+ (3,'Cave de Ribeauvillé'),
+ (4,'Chassagnoux'),
+ (5,'Corton André'),
+ (6,'Codivia');
+
 INSERT INTO `produit` (`IDPRODUIT`,`QUANTITECOMMANDES`,`IDFOURNISSEUR`) VALUES 
  (1,12,1),
  (2,24,2),
@@ -9,10 +17,3 @@ INSERT INTO `produit` (`IDPRODUIT`,`QUANTITECOMMANDES`,`IDFOURNISSEUR`) VALUES
  (6,60,5),
  (7,60,6);
 
-INSERT INTO `fournisseur` (`IDFOURNISSEUR`,`LIBELLEFOURNISSEUR`) VALUES 
- (1,'Chantovent'),
- (2,'Castel'),
- (3,'Cave de Ribeauvillé'),
- (4,'Chassagnoux'),
- (5,'Corton André'),
- (6,'Codivia');
