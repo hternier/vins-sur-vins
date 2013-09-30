a26bfad339e6967844a8191de371ffaf071b7303
CinhicM@gmail.com <CinhicM@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-09-30 08:44:45 +0000

    Déplacement des scripts sql

--- gestionStockInterne/gestionStockInterne-persistance/src/main/resources/scriptsSQL/insertSQL.sql
+++
@@ -1,9 +0,0 @@
-use atod18_g1_geststockint;
-
-insert into `produitstock` values(1,34,30);
-insert into `produitstock` values(2,24,30);
-insert into `produitstock` values(3,256,5);
-insert into `produitstock` values(4,30,15);
-insert into `produitstock` values(5,10,30);
-insert into `produitstock` values(6,253,30);
-insert into `produitstock` values(7,300,30);
