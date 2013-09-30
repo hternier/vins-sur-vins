5e9f9e678356c115e34619de30d9d7ab26cfe745
nicolas.chalet@gmail.com <nicolas.chalet@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-09-30 13:01:32 +0000

    Pour tester les webServices

---
+++ testWebService/src/main/java/testWebService/Test.java
@@ -0,0 +1,23 @@
+package testWebService;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import fr.afcepf.atod18.controleDeStock.entitees.CommandeControleStock;
+import fr.afcepf.atod18.controleDeStock.entitees.ProduitControleStock;
+import fr.afcepf.atod18.controleDeStock.ws.ControleDeStockService;
+
+public class Test {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ControleDeStockService controleDeStockService = ctx.getBean("controleDeStockServicePourTest", ControleDeStockService.class);
+		ProduitControleStock produit = new ProduitControleStock(1);
+		produit.setQuantite(5);
+		CommandeControleStock commande = new CommandeControleStock();
+		commande.getProduits().add(produit);
+		controleDeStockService.passerCommande(commande);
+	}
+
+}
