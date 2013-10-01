70d2d61bafb4ef73cb90615db1ca8e69c743095d
nicolas.chalet@gmail.com <nicolas.chalet@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-10-01 07:52:22 +0000

    ajout de dummy test

---
+++ controleDeStock/controleDeStock-commun/src/test/java/fr/afcepf/atod18/controleDeStock/tests/DummyTest.java
@@ -0,0 +1,13 @@
+package fr.afcepf.atod18.controleDeStock.tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DummyTest {
+	
+	@Test
+	public void testDummy() {
+		assertNotNull("lala");
+	}
+	
+}
