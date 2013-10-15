8dad6a4ad55cee586ab3fa63a9244e9036097465
hadrien.ternier@gmail.com <hadrien.ternier@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-10-15 13:12:29 +0000

    Ajout du framework à testFramework

--- trunk/testFramework/src/main/java/fr/afcepf/atod18/testFramework/controller/ActionLogin.java
+++ trunk/testFramework/src/main/java/fr/afcepf/atod18/testFramework/controller/ActionLogin.java
@@ -1,5 +1,23 @@
 package fr.afcepf.atod18.testFramework.controller;
 
-public class ActionLogin {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.afcepf.al18.framework.vingtSurStruts.core.action.Action;
+
+
+
+public class ActionLogin extends Action {
+
+	@Override
+	public String execute(HttpServletRequest paramRequest,
+			HttpServletResponse paramResponse) {
+		
+		ActionLoginForm loginForm = getForm(ActionLoginForm.class);
+		
+		return "/success.jsp";
+	}
+
+
 
 }
--- trunk/testFramework/src/main/java/fr/afcepf/atod18/testFramework/controller/ActionLoginForm.java
+++ trunk/testFramework/src/main/java/fr/afcepf/atod18/testFramework/controller/ActionLoginForm.java
@@ -1,13 +1,33 @@
 package fr.afcepf.atod18.testFramework.controller;
 
-public class ActionLoginForm {
+import java.util.ArrayList;
+import java.util.List;
 
-    private String login;
-    private String password;
-    
-    
-    
-    
+import javax.servlet.http.HttpServletRequest;
+
+import fr.afcepf.al18.framework.vingtSurStruts.core.action.ActionForm;
+
+public class ActionLoginForm extends ActionForm {
+
+	private static final long serialVersionUID = 1L;
+	private String login;
+	private String password;
+	
+	
+    @Override
+	public String[] validate(HttpServletRequest paramRequest) {
+    	
+    	List<String> message = new ArrayList<String>();
+    	
+    	if (!this.getLogin().equals("toto") || !this.getPassword().equals("toto")) {
+    		// Redirection Erreur
+    		message.add("Error login");
+        } else {
+        	// Redirection Success
+        }
+    	
+    	return message.toArray(new String[message.size()]);
+	}
     
     
     /*
