420011e7773aa64a56ad6fa9ab2b0193d7754983
hadrien.ternier@gmail.com <hadrien.ternier@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-10-15 10:57:35 +0000

    Commit principal du code

---
+++ trunk/testFramework/src/main/java/fr/afcepf/atod18/testFramework/controller/ActionLoginForm.java
@@ -0,0 +1,31 @@
+package fr.afcepf.atod18.testFramework.controller;
+
+public class ActionLoginForm {
+
+    private String login;
+    private String password;
+    
+    
+    
+    
+    
+    
+    /*
+     * Setter et Getter
+     */
+    
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String paramLogin) {
+		login = paramLogin;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String paramPassword) {
+		password = paramPassword;
+	}
+    
+    
+}
