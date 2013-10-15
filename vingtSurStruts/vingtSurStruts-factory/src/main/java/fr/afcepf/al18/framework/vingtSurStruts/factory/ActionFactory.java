41badfdb998a6ee904979bed8dea6c36a3dbd7fc
nicolas.chalet@gmail.com <nicolas.chalet@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-10-15 13:47:45 +0000

    Ajout action form factory

--- vingtSurStruts/vingtSurStruts-factory/src/main/java/fr/afcepf/al18/framework/vingtSurStruts/factory/ActionFactory.java
+++ vingtSurStruts/vingtSurStruts-factory/src/main/java/fr/afcepf/al18/framework/vingtSurStruts/factory/ActionFactory.java
@@ -3,14 +3,6 @@ package fr.afcepf.al18.framework.vingtSurStruts.factory;
 import fr.afcepf.al18.framework.vingtSurStruts.core.action.Action;
 
 public class ActionFactory {
-
-	private final static ActionFactory INSTANCE = new ActionFactory();
-	
-	private ActionFactory(){}
-
-	public static ActionFactory getInstance() {
-		return INSTANCE;
-	}
 	
 	public Action getAction(String actionName) {
 		Action returnAction;
