1a5e2080af38d11332f2a86aa2ce71c4bfd5254d
hadrien.ternier@gmail.com <hadrien.ternier@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-10-17 09:57:10 +0000

    Ajout de l'affichge des message d'erreur en cas de non validation

--- trunk/testFramework/src/main/java/fr/afcepf/atod18/testFramework/controller/ActionLoginForm.java
+++ trunk/testFramework/src/main/java/fr/afcepf/atod18/testFramework/controller/ActionLoginForm.java
@@ -21,7 +21,7 @@ public class ActionLoginForm extends ActionForm {
     	
     	if (this.getLogin().isEmpty() || this.getPassword().isEmpty()) {
     		// Redirection Erreur
-    		message.add("Error login");
+    		message.add("Login ou mot de passe manquant");
         } else {
         	// Redirection Success
         }
