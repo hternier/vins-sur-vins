37949c54b60b01e931f3cd0f2f1a7577eb44de7e
hadrien.ternier@gmail.com <hadrien.ternier@gmail.com@f128a0ce-85a6-21c9-1ecc-4e8e8ee3c4b9>
2013-10-18 13:53:09 +0000

    Adaptation de vin sur vin aux évolution du framework

--- trunk/VinSurVin/src/fr/afcepf/atod17/vinsurvin/framework/controller/ActionNewsletterForm.java
+++ trunk/VinSurVin/src/fr/afcepf/atod17/vinsurvin/framework/controller/ActionNewsletterForm.java
@@ -5,16 +5,13 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import fr.afcepf.al18.framework.vingtSurStruts.configuration.annotations.Form;
-import fr.afcepf.al18.framework.vingtSurStruts.core.action.ActionForm;
+import fr.afcepf.al18.framework.vingtSurStruts.commons.annotations.Form;
+import fr.afcepf.al18.framework.vingtSurStruts.commons.entities.ActionForm;
 
 @Form("newsletterForm")
 public class ActionNewsletterForm  extends ActionForm {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8344489819914049899L;
+	private static final long serialVersionUID = 1L;
 	private String email;
 	
 	
@@ -22,14 +19,11 @@ public class ActionNewsletterForm  extends ActionForm {
 	public String[] validate(HttpServletRequest paramRequest) {
     	
     	List<String> message = new ArrayList<String>();
-		String regexMail = "^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)+$";
 
     	if (this.getEmail().isEmpty()) {
     		// Redirection Erreur
     		message.add("Le champ email ne peut pas être vide.");
 
-//        } else if (!this.getEmail().matches(regexMail)){
-//        	message.add("Le format de l'adresse mail n'est pas valide. Veuillez rééssayer.");
         
         } else {
         	// Redirection Success
